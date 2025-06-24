cc44ab5fa221e778947abc6f2afe9cd1ef05ebab
Aveekal <aveekal@gmail.com>
2025-06-24 11:56:34 +0530

    fix tf.log

--- sensors/utils/loss.py
+++ sensors/utils/loss.py
@@ -13,5 +13,5 @@ def focal_loss(y_true, y_pred, alpha=0.25, gamma=2.0):
     pt = tf.where(tf.equal(y_true, 1), y_pred, 1 - y_pred)
     alpha_t = tf.where(tf.equal(y_true, 1), alpha, 1 - alpha)
     
-    focal_loss = -alpha_t * tf.pow(1 - pt, gamma) * tf.log(pt)
+    focal_loss = -alpha_t * tf.pow(1 - pt, gamma) * tf.math.log(pt)
     return tf.reduce_mean(focal_loss)
